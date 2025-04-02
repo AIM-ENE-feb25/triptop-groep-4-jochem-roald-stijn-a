666a7dcfa0f6c8e19cdc5446bb1ab7109b1093de
Jochem Jansen <jd.jansen2@student.han.nl>
2025-04-02 17:45:55 +0200

    JourneyService geïmplementeerd

--- opdrachten/jochem-prototype/triptop/src/main/java/prototype/jochem/triptop/service/JourneyServiceImpl.java
+++ opdrachten/jochem-prototype/triptop/src/main/java/prototype/jochem/triptop/service/JourneyServiceImpl.java
@@ -1,9 +1,12 @@
 package prototype.jochem.triptop.service;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.stereotype.Service;
 import prototype.jochem.triptop.domain.Journey;
 import prototype.jochem.triptop.domain.Transport;
+import prototype.jochem.triptop.strategy.AlternativeJourneyStrategy;
 import prototype.jochem.triptop.strategy.JourneyStrategy;
+import prototype.jochem.triptop.strategy.StandardJourneyStrategy;
 
 import java.util.List;
 
@@ -12,12 +15,21 @@ public class JourneyServiceImpl implements JourneyService {
     JourneyStrategy journeyStrategy;
 
     @Override
-    public void setJourneyStrategy(JourneyStrategy journeyStrategy) {
-        this.journeyStrategy = journeyStrategy;
+    public void setJourneyStrategy(String journeyStrategy) {
+        switch (journeyStrategy) {
+            case "StandardJourney":
+                this.journeyStrategy = new StandardJourneyStrategy();
+                break;
+            case "AlternativeJourney":
+                this.journeyStrategy = new AlternativeJourneyStrategy();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid journey strategy: " + journeyStrategy);
+        }
     }
 
     @Override
-    public List<Journey> getJourneys(String origin, String destination, String departureDate, String returnDate, double price, Transport transport) {
-        return List.of();
+    public List<Journey> getJourneys(String origin, String destination, String departureDate, String returnDate, double price, Transport transport) throws UnirestException {
+        return journeyStrategy.getJourneys(origin, destination, departureDate, returnDate, price, transport);
     }
 }
